173dedc414356cf675826bd63831ced1dbd8c44a
Gioele Muscolino <gioele.muscolino@gmail.com>
2025-11-03 15:17:26 +0100

    - fixed commands enum

--- src/common/Commands.java
+++ src/common/Commands.java
@@ -7,22 +7,10 @@ public class Commands {
     }
 
     public static String getClientCommand(EClientToServerCommands command){
-        switch(command){
-            case EXIT -> { return "EXIT"; }
-            case STATUS -> { return "STATUS"; }
-            case MESSAGE -> { return "MESSAGE"; }
-            case BID -> { return "BID"; }
-            case REGISTER -> { return "REGISTER"; }
-            default -> { return "HELP"; }
-        }
+        return command.name();
     }
 
     public static String getServerCommand(EServerToClientCommands command){
-        switch(command){
-            case USER_REGISTER_SUCCESS -> { return "USER_REGISTER_SUCCESS"; }
-            case USER_REGISTER_FAIL -> { return "USER_REGISTER_FAIL"; }
-            case IGNORE -> { return "IGNORE"; }
-            default -> { return "PRINT_MESSAGE"; }
-        }
+        return command.name();
     }
 }
